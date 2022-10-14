472124d17a3e860dab40dba3452cbe6cf6417403
JmDeleon2000 <54918136+JmDeleon2000@users.noreply.github.com>
2022-10-14 16:28:53 -0600

    compresión de fotos

--- cleanup.py
+++ cleanup.py
@@ -10,35 +10,20 @@ if not os.path.isdir('jpgs'):
 
 dims = (1000, 1000)
 
-zip = zipfile.ZipFile('data/second.zip')
+zip = zipfile.ZipFile('data/mayo-clinic-strip-ai.zip')
 
 #Te prometo que no es una zipbomb
 Image.MAX_IMAGE_PIXELS = None
 
-def boundingBox(img):
-    x = img.size[0]//2
 
-    y_lower = img.size[1]//4
-    y_upper = img.size[1]//2
 
-    min = 240
-    while True:
-        y = (y_upper-y_lower)//2
-        a = img.getpixel((x, y))
-        if a > min:
-            y_lower = y
-        else:
-            for i in range(0, img.size[0], img.size[0]//10):
-                a = img.getpixel((i, y))
-                if a <= min: 
-                    y_lower = y
-                    continue
-            return (x, y)
-    return img.size
 
+regex= r'train/(\w*)[.tif$|.tiff$]'
 for i in zip.namelist():
-    
-    filename = re.findall(r'\b/(.*?)[.tif$|.tiff$]', i)[0]
+    if not re.match(regex, i):
+        continue
+
+    filename = re.findall(regex, i)[0]
     newfilename = f'jpgs/{filename}.jpg'
     if os.path.isfile(newfilename):
         continue
