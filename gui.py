58778969feb47f4185f6502327a6ae03b61a41c6
coutino568 <40869460+coutino568@users.noreply.github.com>
2022-11-18 13:11:16 -0600

    v3
    lee el input del archivo

--- gui.py
+++ gui.py
@@ -10,30 +10,35 @@ from PIL import Image
 color_base= '#2776c1'
 color_fondo_secundario= '#70a7db'
 color_secundario= '#fb8300'
-
+button1text= "Classify Using model 1"
+button2text= "Classify Using model 2"
+globalfile=""
 
 layout1 = [
 	[
 	sg.Text("Seleccionar Imagen"),
-	sg.In(size = (21,1), enable_events=True, key="-FOLDER-" ,background_color=color_fondo_secundario),
-	sg.FolderBrowse(),
+	sg.In(size = (21,1), enable_events=True, key="-FILE-" ,background_color=color_fondo_secundario),
+	# sg.FolderBrowse(),
+	sg.FileBrowse(key="-FILE-"),
 
-
-	],
-	[
-		sg.Listbox(
-				values = [] , enable_events=True, size =(40,20),
-				key="-FILE_LIST-"
-			)
-
-	],
+	],[sg.HSeparator()],
+	[sg.Image(key="-IMAGE-")]
+	
 	
 ]
 
 layout2 = [
 	[sg.Text("Seleccionar el modelo")],
-	[sg.Image(key="-IMAGE-")],
-	[sg.Button("Classify")]
+	# [sg.Text("Seleccionar el modelo" , key="filename")],
+	[sg.Text("Modelo 1: 85% Accuracy")],
+	[sg.Button(button1text)],
+	[sg.Text("Modelo 2: 52% Accuracy")],
+	[sg.Button(button2text)],
+
+	[sg.Text("OUTPUT:")],
+	[sg.Text("",key="-OUTPUT-")],
+
+
 
 
 	]	
@@ -54,8 +59,25 @@ mywindow = sg.Window(title="Classifier", layout=layout, margins=(100,50) , backg
 while True:
 	event, values = mywindow.read()
 
-	if event == "Classify" :
-		print("Boton presionado")
+	if event == button1text:
+		print("Boton 1 presionado")
+		print("Enviare este file al modelo 1 : " +globalfile)
+		mywindow["-OUTPUT-"].update("resultado del modelo1") 
+	if event == button2text :
+		print("Boton 2 presionado")
+		print("Enviare este file al modelo 2 : " +globalfile)
+		mywindow["-OUTPUT-"].update("resultado del modelo2") 
+
+
+	if event == "-FILE-" :
+		print("Se selecciono una archivo")
+		file= values["-FILE-"]
+		print(file)
+		globalfile=file
+		#mywindow["filename"].update(file)
+		mywindow["-IMAGE-"].update(filename=file)
+
+
 
 	if event == sg.WIN_CLOSED:
 		break
@@ -76,6 +98,8 @@ while True:
 	if event =="-FILE_LIST-":
 		try:
 			filename=os.path.join (values["-FOLDER-"], values["-FILE_LIST-"][0])
+			filename = values["-FILE-"]
+			#mywindow["filename"].update(filename)
 			mywindow["-IMAGE-"].update(filename=filename)
 		except:
 			pass
